f23d44dcd02ef366ab6acbd2fac20b17d4b7fd9d
LavenderEdit <gercermagden@gmail.com>
2025-11-10 16:29:21 -0500

    feat(social-link): mejorar la entidad SocialLink
    Se han realizado las siguientes mejoras en la clase `SocialLink`:
    
    - Se ha añadido una relación `@ManyToOne` con la entidad `Profile`, utilizando la columna `profile_id` como clave foránea, para asociar cada enlace social a un perfil específico. Se configuró la carga perezosa (`fetch = FetchType.LAZY`) y se ha excluido esta propiedad del método `toString` para evitar posibles recursiones.
    - Se ha añadido la anotación `@ToString` a la clase para generar automáticamente el método `toString()`, mejorando la legibilidad de la entidad y facilitando la depuración.
    - Se ha añadido el campo `platform` con un tamaño máximo de 50 caracteres para almacenar el nombre de la plataforma del enlace social (por ejemplo, "Facebook", "LinkedIn").
    - Se ha añadido el campo `url` con un tamaño máximo de 512 caracteres para almacenar la URL completa del enlace social.
    - Se ha añadido el campo `sortOrder` para gestionar el orden de los enlaces sociales en la interfaz de usuario, permitiendo un control más fino sobre su presentación.
    
    Estos cambios mejoran la estructura de la entidad `SocialLink`, permitiendo asociar enlaces sociales a un perfil específico, almacenar la URL de forma eficiente y controlar el orden de visualización de los enlaces en la interfaz.

--- src/main/java/studios/tkoh/portfolio/model/SocialLink.java
+++ src/main/java/studios/tkoh/portfolio/model/SocialLink.java
@@ -6,12 +6,14 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.io.Serializable;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 import studios.tkoh.portfolio.audit.BaseEntity;
 
 /**
@@ -19,20 +21,27 @@ import studios.tkoh.portfolio.audit.BaseEntity;
  * @author Studios TKOH!
  */
 @Entity
-@Table(name = "social_link")
+@Table(name = "social_link", schema = "studiostkoh.portafolio")
 @Getter
 @Setter
 @NoArgsConstructor
+@ToString
 public class SocialLink extends BaseEntity implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "profile_id", nullable = false, referencedColumnName = "id")
+    @ToString.Exclude
     private Profile profile;
+
     @Column(nullable = false, length = 50)
     private String platform;
+
     @Column(nullable = false, length = 512)
     private String url;
+
     private int sortOrder;
 }
