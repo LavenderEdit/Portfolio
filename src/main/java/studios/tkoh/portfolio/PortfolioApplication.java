1e50454ab40274abc46cc0dbf9e497726d499434
LavenderEdit <gercermagden@gmail.com>
2025-11-10 17:16:13 -0500

    feat(config): habilitar auditoría JPA con configuración personalizada
    Se ha añadido la configuración para habilitar la auditoría JPA en la aplicación. 
    Se incorporaron las anotaciones `@EnableJpaAuditing` y el método `auditorAware` 
    que devuelve una instancia de `ApplicationAuditAware` para gestionar los auditores de 
    la aplicación de manera personalizada. Esto permite registrar la información de auditoría 
    en las entidades JPA durante las operaciones de persistencia.

--- src/main/java/studios/tkoh/portfolio/PortfolioApplication.java
+++ src/main/java/studios/tkoh/portfolio/PortfolioApplication.java
@@ -2,16 +2,25 @@ package studios.tkoh.portfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import studios.tkoh.portfolio.config.ApplicationAuditAware;
 
 /**
  *
  * @author Studios TKOH!
  */
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 public class PortfolioApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PortfolioApplication.class, args);
     }
 
+    @Bean
+    public AuditorAware<String> auditorAware() {
+        return new ApplicationAuditAware();
+    }
 }
