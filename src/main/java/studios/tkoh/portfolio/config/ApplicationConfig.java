25f74f22858d141d90c6b912bc149454ef4b41d9
LavenderEdit <gercermagden@gmail.com>
2025-11-10 22:49:51 -0500

    refactor: eliminar configuración personalizada de UserDetailsService
    Se eliminó la configuración personalizada de `UserDetailsService` en `ApplicationConfig`, que previamente se encargaba de cargar los detalles del usuario desde el repositorio `UserRepository`.
    
    - El método `userDetailsService()` que usaba `usuarioRepository.findByEmail()` para cargar un usuario a partir de su email y lanzar un `UsernameNotFoundException` si no se encontraba fue eliminado.
    - Esta refactorización sugiere que la carga de los detalles del usuario ahora se maneja de manera diferente, probablemente utilizando la configuración predeterminada de Spring Security para el manejo de usuarios.
    - También se eliminó la dependencia de `UserRepository` y la clase `UsernameNotFoundException` ya no es necesaria.
    - El `PasswordEncoder` (BCryptPasswordEncoder) sigue estando configurado como un bean.
    
    Este cambio podría ser parte de una reestructuración más amplia del manejo de autenticación y autorización, posiblemente moviendo la lógica de la carga de usuarios a un componente de seguridad más genérico o usando un mecanismo diferente de autenticación.

--- src/main/java/studios/tkoh/portfolio/config/ApplicationConfig.java
+++ src/main/java/studios/tkoh/portfolio/config/ApplicationConfig.java
@@ -8,10 +8,8 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import studios.tkoh.portfolio.repository.UserRepository;
 
 /**
  *
@@ -20,14 +18,6 @@ import studios.tkoh.portfolio.repository.UserRepository;
 @Configuration
 @RequiredArgsConstructor
 public class ApplicationConfig {
-
-    private final UserRepository usuarioRepository;
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-        return username -> usuarioRepository.findByEmail(username)
-                .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado con email: " + username));
-    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
