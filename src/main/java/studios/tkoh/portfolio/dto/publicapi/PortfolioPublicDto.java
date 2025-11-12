698d6f506de834326fdb8e1e34578023509c9824
LavenderEdit <gercermagden@gmail.com>
2025-11-12 11:06:38 -0500

    feat(dto): agregar DTO para detalles públicos resumidos del portafolio
    Se añadió un nuevo DTO llamado `PortfolioPublicDto` para representar un conjunto reducido de datos del portafolio. Este DTO incluye propiedades como el 'slug', el nombre completo, el encabezado y la URL del avatar, lo que permite exponer una versión resumida del portafolio en la API pública.

---
+++ src/main/java/studios/tkoh/portfolio/dto/publicapi/PortfolioPublicDto.java
@@ -0,0 +1,13 @@
+package studios.tkoh.portfolio.dto.publicapi;
+
+/**
+ *
+ * @author Studios TKOH!
+ */
+public record PortfolioPublicDto(
+        String slug,
+        String fullName,
+        String headline,
+        String avatarUrl) {
+
+}
