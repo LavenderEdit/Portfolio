3d6b8887108e148006900ede26dd401357dc577e
LavenderEdit <gercermagden@gmail.com>
2025-11-11 23:22:28 -0500

    feat(service): crea interfaz UploadService para manejo de cargas de archivos
    Se define la interfaz UploadService con métodos para subir avatares, currículums,
    portadas de proyectos e íconos de habilidades, lanzando excepciones en caso de error
    de E/S o seguridad.

---
+++ src/main/java/studios/tkoh/portfolio/service/UploadService.java
@@ -0,0 +1,23 @@
+package studios.tkoh.portfolio.service;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import org.springframework.web.multipart.MultipartFile;
+import studios.tkoh.portfolio.dto.profile.ProfileDto;
+import studios.tkoh.portfolio.dto.project.ProjectDto;
+import studios.tkoh.portfolio.dto.skill.SkillDto;
+
+/**
+ *
+ * @author Studios TKOH!
+ */
+public interface UploadService {
+
+    ProfileDto uploadAvatar(MultipartFile file) throws IOException, GeneralSecurityException;
+
+    ProfileDto uploadResume(MultipartFile file) throws IOException, GeneralSecurityException;
+
+    ProjectDto uploadProjectCover(Long projectId, MultipartFile file) throws IOException, GeneralSecurityException;
+
+    SkillDto uploadSkillIcon(Long skillId, MultipartFile file) throws IOException, GeneralSecurityException;
+}
