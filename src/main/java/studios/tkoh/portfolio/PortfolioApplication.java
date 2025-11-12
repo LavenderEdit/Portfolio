58734d67093080ecccdace314981af9bc989093a
LavenderEdit <gercermagden@gmail.com>
2025-11-12 09:46:33 -0500

    feat(config): habilitar ejecución asincrónica con @EnableAsync
    Se añadió la anotación @EnableAsync a la clase PortfolioApplication para permitir la ejecución de métodos asincrónicos en la aplicación. Esto habilita el uso de @Async en los servicios que lo requieran, mejorando el rendimiento de las tareas paralelizadas.

--- src/main/java/studios/tkoh/portfolio/PortfolioApplication.java
+++ src/main/java/studios/tkoh/portfolio/PortfolioApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 import studios.tkoh.portfolio.config.ApplicationAuditAware;
 
 /**
@@ -13,6 +14,7 @@ import studios.tkoh.portfolio.config.ApplicationAuditAware;
  */
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditorAware")
+@EnableAsync
 public class PortfolioApplication {
 
     public static void main(String[] args) {
