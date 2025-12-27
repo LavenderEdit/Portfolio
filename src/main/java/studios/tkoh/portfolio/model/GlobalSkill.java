f65ebf625f9a5c4f74d0f2007cd20c08cf9dc4c3
LavenderEdit <gercermagden@gmail.com>
2025-12-27 09:30:14 -0500

    feat(model): crear entidad GlobalSkill

---
+++ src/main/java/studios/tkoh/portfolio/model/GlobalSkill.java
@@ -0,0 +1,37 @@
+package studios.tkoh.portfolio.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.io.Serializable;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import studios.tkoh.portfolio.audit.BaseEntity;
+
+/**
+ *
+ * @author Studios TKOH!
+ */
+@Entity
+@Table(name = "global_skill", schema = "studiostkoh.portafolio")
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class GlobalSkill extends BaseEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 80)
+    private String name;
+
+    @Column(name = "icon_url", length = 512)
+    private String iconUrl;
+}
