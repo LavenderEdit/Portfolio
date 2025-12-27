82eebf491b699ed5f38261c5f23a8b69929addb2
LavenderEdit <gercermagden@gmail.com>
2025-12-27 09:30:55 -0500

    feat(dto): crear GlobalSkillDto

---
+++ src/main/java/studios/tkoh/portfolio/dto/skill/GlobalSkillDto.java
@@ -0,0 +1,12 @@
+package studios.tkoh.portfolio.dto.skill;
+
+/**
+ *
+ * @author Studios TKOH!
+ */
+public record GlobalSkillDto(
+        Long id,
+        String name,
+        String iconUrl) {
+
+}
